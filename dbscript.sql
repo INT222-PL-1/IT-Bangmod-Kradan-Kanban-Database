7f1f8fcf3479867fadf66df9030627126e640774
Sittha Manittayakul <zyxma.6666@hotmail.com>
2024-09-16 01:34:23 +0700

    Update dbscript.sql

--- dbscript.sql
+++ dbscript.sql
@@ -33,19 +33,19 @@ CREATE TABLE taskV1 (
     PRIMARY KEY (taskId)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO taskV1 (
-    taskTitle,
-    taskDescription,
-    taskAssignees,
-    taskStatus,
-    createdOn,
-    updatedOn
-)
-VALUES 
-('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0','Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5','Assignees1Assignees2Assignees3','NO_STATUS','2024-04-22 09:00:00','2024-04-22 09:00:00'),
-('Repository', null, null,'TO_DO','2024-04-22  09:05:00','2024-04-22 14:00:00'),
-('ดาต้าเบส','ສ້າງຖານຂໍ້ມູນ','あなた、彼、彼女 (私ではありません)','DOING','2024-04-22  09:10:00','2024-04-25 00:00:00'),
-('_Infrastructure_','_Setup containers_','ไก่งวง กับ เพนกวิน','DONE','2024-04-22 09:15:00','2024-04-22 10:00:00');
+-- INSERT INTO taskV1 (
+--     taskTitle,
+--     taskDescription,
+--     taskAssignees,
+--     taskStatus,
+--     createdOn,
+--     updatedOn
+-- )
+-- VALUES 
+-- ('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0','Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5','Assignees1Assignees2Assignees3','NO_STATUS','2024-04-22 09:00:00','2024-04-22 09:00:00'),
+-- ('Repository', null, null,'TO_DO','2024-04-22  09:05:00','2024-04-22 14:00:00'),
+-- ('ดาต้าเบส','ສ້າງຖານຂໍ້ມູນ','あなた、彼、彼女 (私ではありません)','DOING','2024-04-22  09:10:00','2024-04-25 00:00:00'),
+-- ('_Infrastructure_','_Setup containers_','ไก่งวง กับ เพนกวิน','DONE','2024-04-22 09:15:00','2024-04-22 10:00:00');
 
 
 
